aa28f38a28a4a4f3c9146f15472ad03b3ba588c0
Christof Harnischmacher <ch@cm4all.com>
2020-05-20 23:26:01 +0000

    Brew formula update for xhdmi-tools version 0.0.18

--- Formula/xhdmi-tools.rb
+++ Formula/xhdmi-tools.rb
@@ -2,24 +2,24 @@
 class XhdmiTools < Formula
   desc "XHDMI-Tools, e.g. for creating custom fonts and gamma/color maps"
   homepage "https://gitlab.com/chriz2600/xhdmi-tools"
-  version "0.0.17"
+  version "0.0.18"
   bottle :unneeded
 
   if OS.mac?
-    url "https://gitlab.com/chriz2600/xhdmi-tools/uploads/a53fd8bea07f0919f2359e56b7bab69d/xhdmi-tools_0.0.17_darwin_amd64.tar.gz"
-    sha256 "4428ed7d43a22e581a6f96f8707c19f2eba8caab533c980ab55accfaefd7e778"
+    url "https://gitlab.com/chriz2600/xhdmi-tools/uploads/2d0e6b254aca07520c6ee822fce2111b/xhdmi-tools_0.0.18_darwin_amd64.tar.gz"
+    sha256 "640035f2fdce68567059c551447481f8f5c0b4ce6b1b2052599d21d63f77c7ad"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://gitlab.com/chriz2600/xhdmi-tools/uploads/6f7e3dd2979f2af5f64e85ecd52b5e92/xhdmi-tools_0.0.17_linux_amd64.tar.gz"
-      sha256 "cb80a360605bfd5a1655d5885133e551909b02ca35e5bd2ff046e7f76d0fc602"
+      url "https://gitlab.com/chriz2600/xhdmi-tools/uploads/cb1e6dfad7bad204a165d843b3d61220/xhdmi-tools_0.0.18_linux_amd64.tar.gz"
+      sha256 "1b7ee83137805632b64e0ba8367129dcc3cd5b97c53417a1db0c284167ae3da0"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://gitlab.com/chriz2600/xhdmi-tools/uploads/4a420a5dcc13aee879081c1c84e29e8c/xhdmi-tools_0.0.17_linux_arm64.tar.gz"
-        sha256 "081832f32bbdcbbb8fa71af0f842679f0e938a99f8b9c4ea6b622622e16e5876"
+        url "https://gitlab.com/chriz2600/xhdmi-tools/uploads/1de44c1594e1adefe2b19e9a7dba6a03/xhdmi-tools_0.0.18_linux_arm64.tar.gz"
+        sha256 "aacf821c70d51ef1c3ca838a23537dc0453803f7b526357988530b72590f8355"
       else
-        url "https://gitlab.com/chriz2600/xhdmi-tools/uploads/1ddf68f7e63649946244321c1e568a35/xhdmi-tools_0.0.17_linux_arm.tar.gz"
-        sha256 "50bcfff2eb1b50301d357c52c7c3417aca62185fb58084b788b8c4dcfe4462f5"
+        url "https://gitlab.com/chriz2600/xhdmi-tools/uploads/7e51cd963711f38d98886a6bc05eae72/xhdmi-tools_0.0.18_linux_arm.tar.gz"
+        sha256 "2b8c241974697ee9a3a54b29cf46626b8ea68f4efac4ab70f9642026727127ac"
       end
     end
   end
